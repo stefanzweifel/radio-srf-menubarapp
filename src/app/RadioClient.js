05779d444e723de163ea9a6553080d18193897db
Stefan Zweifel <hello@stefanzweifel.io>
2016-06-26 16:15:16 +0200

    Fix Playback Error
    Signed-off-by: Stefan Zweifel <hello@stefanzweifel.io>

--- src/app/RadioClient.js
+++ src/app/RadioClient.js
@@ -6,7 +6,8 @@ export default class RadioClient {
 
     constructor() {
         this.currentlyPlaying = null;
-        this.isPlaying = false;
+        this.isPlaying        = false;
+        this.speaker          = new Speaker();
     }
 
     stream(url) {
@@ -21,8 +22,6 @@ export default class RadioClient {
 
         icy.get(url, (res) => {
 
-            // console.error(res.headers);
-
             // log any "metadata" events that happen
             // TODO: Would be nice if this information could be displayed in
             // the menubar or the application menu.
@@ -34,7 +33,7 @@ export default class RadioClient {
             });
 
             this.currentlyPlaying = res;
-            res.pipe(new lame.Decoder()).pipe(new Speaker());
+            res.pipe(new lame.Decoder()).pipe(this.speaker);
             this.isPlaying = true;
         });
     }
@@ -46,8 +45,11 @@ export default class RadioClient {
      */
     stop() {
         if (this.isPlaying === true) {
-            this.currentlyPlaying.res.client.destroy();
+            this.speaker.close(); // Mute Sound
+            this.currentlyPlaying.res.client.destroy(); // Close Connection
+
             this.isPlaying = false;
+            this.speaker = new Speaker();
         }
     }
 
